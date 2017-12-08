3057bb054981249323f32d60c1945e815b7a9b34
vvyadrincev <vvyadrincev@gmail.com>
2017-12-08 21:40:04 +0300

    Revert "Revert "first commit""
    This reverts commit 217f7284c8171793ba117ecd6de1a24dbf213273.

---
+++ 1_predobrabotka-dannykh-v-pandas/utils.py
@@ -0,0 +1,37 @@
+import re
+import os
+import logging
+
+
+FORMAT="%(asctime)s %(levelname)s: %(name)s: %(message)s"
+logging.basicConfig(level=logging.INFO, format = FORMAT)
+
+
+def confirm(answer="OK to push to continue [Y/N]? "):
+    """
+    Ask user to enter Y or N (case-insensitive).
+    :return: True if the answer is Y.
+    :rtype: bool
+    """
+    while answer not in ["y", "n"]:
+        answer = raw_input(answer + " [Y/N]").lower()
+    return answer == "y"
+
+
+def save_txt_fo_file (file, txt, rewrite = False):
+    if ( not os.path.exists(file) or rewrite ):
+        with open (file, 'w') as f:
+            f.write (txt)
+            logging.info ("Saved '%s' to '%s'" % (txt, file))
+    else:
+        save_txt_fo_file (file, txt, confirm ("Rewrite '%s'?" % file) )
+    
+    
+def save_answ (number, answer, folder = 'answers'):
+    if os.path.isdir(folder):
+        file = os.path.join(folder, str(number) + "_answ.txt")
+        save_txt_fo_file(file, answer)
+    else:
+        os.mkdir(folder)
+        logging.info ("Created folder %s", folder)
+        save_answ (number, answer, folder)
